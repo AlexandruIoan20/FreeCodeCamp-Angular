560457ecfaedcbea4be114f4e06a22abe5833386
AlexandruIoan20 <ialexmoraru@yahoo.ro>
2024-12-08 15:12:43 +0200

    Angular build in directives

--- testapp/src/app/app.component.ts
+++ testapp/src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RoomsComponent } from "./rooms/rooms.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RoomsComponent],
+  imports: [RouterOutlet, RoomsComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 
 export class AppComponent {
   title = 'testapp';
+
+  role = 'Admin'; 
 }
--- testapp/src/app/rooms/rooms.component.ts
+++ testapp/src/app/rooms/rooms.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { Room } from './rooms';
+import { CommonModule } from '@angular/common';
+import { RoomList } from './rooms';
 
 @Component({
   selector: 'app-rooms',
-  imports: [],
+  imports: [ CommonModule ],
   templateUrl: './rooms.component.html',
   styleUrl: './rooms.component.scss'
 })
@@ -11,6 +14,42 @@ export class RoomsComponent {
   numberOfRooms = 10;
 
   hideRooms = false; 
+
+  rooms: Room = { 
+    totalRooms: 20,  
+    availableRooms: 10, 
+    bookedRooms: 5, 
+  }
+
+  roomList: RoomList[] = [ 
+    { 
+      roomNumber: 1, 
+      roomType: "Deluxe Room", 
+      amenities: "Air Conditioner, Free Wi-Fi, Bathroom, Kitchen", 
+      price: 500, 
+      photos: "https://unsplash.com/photos/a-man-sitting-in-a-chair-in-a-hotel-room-beeCdpiiXRk", 
+      checkinTime: new Date('11-Nov-2021'), 
+      checkoutTime: new Date('12-Nov-2021'), 
+    },
+    { 
+      roomNumber: 2, 
+      roomType: "Deluxe Room", 
+      amenities: "Air Conditioner, Free Wi-Fi, Bathroom, Kitchen", 
+      price: 1000, 
+      photos: "https://unsplash.com/photos/a-man-sitting-in-a-chair-in-a-hotel-room-beeCdpiiXRk", 
+      checkinTime: new Date('11-Nov-2021'), 
+      checkoutTime: new Date('12-Nov-2021'), 
+    },
+    { 
+      roomNumber: 3, 
+      roomType: "Private Suite", 
+      amenities: "Air Conditioner, Free Wi-Fi, Bathroom, Kitchen", 
+      price: 1500, 
+      photos: "https://unsplash.com/photos/a-man-sitting-in-a-chair-in-a-hotel-room-beeCdpiiXRk", 
+      checkinTime: new Date('11-Nov-2021'), 
+      checkoutTime: new Date('12-Nov-2021'), 
+    },
+  ]
 
   toggle() { 
     this.hideRooms = !this.hideRooms; 
