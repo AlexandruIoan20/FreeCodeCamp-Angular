6ebf193763241eb2e958904ff86e584cfb9968b1
AlexandruIoan20 <ialexmoraru@yahoo.ro>
2024-12-08 14:14:10 +0200

    Binding syntax

---
+++ testapp/src/app/rooms/rooms.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rooms',
+  imports: [],
+  templateUrl: './rooms.component.html',
+  styleUrl: './rooms.component.scss'
+})
+export class RoomsComponent {
+  hotelName = "Hilton Hotel"; 
+  numberOfRooms = 10;
+
+  hideRooms = false; 
+
+  toggle() { 
+    this.hideRooms = !this.hideRooms; 
+  }
+}
