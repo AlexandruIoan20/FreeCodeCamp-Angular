cc62a4f216b386a2d99d7a63811cce9db8b11e32
AlexandruIoan20 <ialexmoraru@yahoo.ro>
2024-12-09 16:54:38 +0200

    Routing

--- testapp/src/app/app.component.ts
+++ testapp/src/app/app.component.ts
@@ -1,17 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { RoomsComponent } from "./rooms/rooms.component";
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import {HeaderComponent} from './header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RoomsComponent, CommonModule ],
+  imports: [RouterOutlet, CommonModule, RouterLink, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 
 export class AppComponent {
   title = 'testapp';
-
-  role = 'Admin';
 }
--- testapp/src/app/app.routes.ts
+++ testapp/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { RoomsComponent } from './rooms/rooms.component';
+import { EmployeeComponent } from './employee/employee.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'employee', component: EmployeeComponent,
+  },
+  {
+    path: 'rooms', component: RoomsComponent,
+  },
+  {
+    path: '', redirectTo: '/rooms', pathMatch: 'full',
+  }
+];
